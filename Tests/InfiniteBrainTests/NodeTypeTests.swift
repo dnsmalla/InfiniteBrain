a25ca58b2b8e605f9628fadde5f2e06e3452310d
dnsmalla <dnsmalla@gmail.com>
2026-05-23 12:22:38 +0900

    feat(core): add code-graph NodeType constants

--- Tests/InfiniteBrainTests/NodeTypeTests.swift
+++ Tests/InfiniteBrainTests/NodeTypeTests.swift
@@ -2,11 +2,23 @@ import XCTest
 @testable import InfiniteBrainCore
 
 final class NodeTypeTests: XCTestCase {
-    func testAllSixteenNodeTypesPresent() {
-        XCTAssertEqual(NodeType.allCases.count, 16)
+    func testAllTwentyNodeTypesPresent() {
+        XCTAssertEqual(NodeType.allCases.count, 20)
     }
 
     func testAllTenEdgeTypesPresent() {
         XCTAssertEqual(EdgeType.allCases.count, 10)
+    }
+
+    func testCodeGraphNodeTypeConstants() {
+        XCTAssertEqual(NodeType.codeFile.rawValue, "code_file")
+        XCTAssertEqual(NodeType.codeSymbol.rawValue, "code_symbol")
+        XCTAssertEqual(NodeType.codeModule.rawValue, "code_module")
+        XCTAssertEqual(NodeType.docPage.rawValue, "doc_page")
+        let all = NodeType.allCases
+        XCTAssertTrue(all.contains(.codeFile))
+        XCTAssertTrue(all.contains(.codeSymbol))
+        XCTAssertTrue(all.contains(.codeModule))
+        XCTAssertTrue(all.contains(.docPage))
     }
 }
